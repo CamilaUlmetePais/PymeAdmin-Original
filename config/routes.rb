f47cf9d7a5cfbab034e49434bd0672545583059f
Camila Pais <camila.pais97@gmail.com>
2019-11-30 13:06:46 -0300

    updated gitignore to include notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :outflows
-  resources :suppliers
   root to: 'inflows#index'
-  resources :inflows
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :suppliers
+  resources :inflows
+  resources :outflows
 end
