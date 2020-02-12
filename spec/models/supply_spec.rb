3551359da3cf82984ca77ef67d6ddbd4ae603650
Camila <camila@example.com>
2020-02-12 19:52:29 -0300

    testing supply spec

--- spec/models/supply_spec.rb
+++ spec/models/supply_spec.rb
@@ -15,21 +15,42 @@ RSpec.describe Supply, type: :model do
 		it { should validate_uniqueness_of(:name).case_insensitive }
 	end
 
+	context "Cogs and units_bought methods" do
+		it "calculates the cost of goods sold (COGS)" do
+			@supply        = create(:supply, price: 2)
+			@outflow       = create(:outflow)
+			@outflow_item1 = create(:outflow_item, quantity: 5)
+			@outflow_item2 = create(:outflow_item, quantity: 10)
+			@outflow_item3 = create(:outflow_item, quantity: 6)
+
+			@supply.units_bought == 21
+			@supply.cogs         == 42
+		end
+	end
+
+	context "Get_operative_expenses method" do
+		xit "calculates the cost of operative expenses" do
+			@supply.get_operative_expenses
+		end
+	end
+
+	context "Update_stock method" do
+		it "updates the 'stock' attribute" do
+			@supply        = create(:supply, stock: 10)
+			@outflow       = create(:outflow)
+			@outflow_item1 = create(:outflow_item, quantity: 10)
+
+			@supply.update_stock(@outflow_item1.quantity)
+			@supply.stock == 20
+		end
+	end
+
 end
 
 =begin
 
-def cogs
-		self.units_bought * self.price
-	end
-
-	# Supply -> [{keys: supplier_id, value: quantity}]
 	def get_operative_expenses
 		self.suppliers.uniq.map{|supplier| supplier.get_expenses(self.id, self.name)}
-	end
-
-	def units_bought
-		self.outflow_items.sum('quantity')
 	end
 
 	def update_stock(quantity)
