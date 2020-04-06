0a96cf63598a942f6d7154e0e41ef73f4e9c05b8
Camila <camila@example.com>
2020-04-06 13:00:30 -0300

    Fixed method balance_alert

--- app/models/auto_notification.rb
+++ app/models/auto_notification.rb
@@ -1,20 +1,20 @@
 class AutoNotification < Notification
-	def stock_alert(product)
+	def self.stock_alert(product)
 		if product.stock <= product.notification_threshold
 			params = {
 				title: I18n.t('auto_notification.stock_alert.title', product: product.name),
 				text: I18n.t('auto_notification.stock_alert.text', product: product.name, stock: product.stock),
-				due_date: Datetime.now
+				due_date: DateTime.now
 			}
 			self.create(params)
 		end
 	end
 
-	def balance_alert(supplier)
+	def self.balance_alert(supplier)
 		if supplier.account_balance <= supplier.notification_threshold
 			difference = supplier.notification_threshold - supplier.account_balance
 			params     = {
-				due_date: Datetime.now,
+				due_date: DateTime.now,
 				title: I18n.t('auto_notification.balance_alert.title', supplier: supplier.name),
 				text: I18n.t('auto_notification.balance_alert.text',
 					supplier: supplier.name,
