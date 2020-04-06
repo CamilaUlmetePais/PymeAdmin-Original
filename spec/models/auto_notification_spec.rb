9331c1c6cc8bb6c63656920ca8c143d1cdb3dc85
Camila <camila@example.com>
2020-04-06 12:59:31 -0300

    AutoNotification specs done

--- spec/models/auto_notification_spec.rb
+++ spec/models/auto_notification_spec.rb
@@ -3,55 +3,27 @@ require 'rails_helper'
 RSpec.describe AutoNotification, type: :model do
 
 	context "Stock_alert method" do
-		it "creates a notification when a product's stock falls below the stipulated threshold" do
-			@product     = create(:product, stock: 20, notification_threshold: 5)
-			@inflow      = create(:inflow)
-			@inflow_item = create(:inflow_item, quantity: 16)
+		it "creates a notification when a product's stock falls below the threshold" do
+			@product     = create(:product, name: "Pollo", stock: 4, notification_threshold: 5)
 
-			stock_alert(@product)
-			# Expect: create notification
+			@last_notification = AutoNotification.last
+
+			AutoNotification.stock_alert(@product)
+
+			expect(AutoNotification.last).not_to eq(@last_notification)
 		end
 	end
 
 	context "Balance_alert method" do
-		it "creates a notification when a supplier's account_balance falls below the stipulated threshold" do
-			@supplier = create(:supplier, notification_threshold: -100)
-			@outflow  = create(:outflow, total: -120)
+		it "creates a notification when a supplier's account_balance falls below the threshold" do
+			@supplier = create(:supplier, account_balance: -120, notification_threshold: -100)
 
-			balance_alert(@supplier)
-			# Expect: create notification
+			@last_notification = AutoNotification.last
+
+			AutoNotification.balance_alert(@supplier)
+
+			expect(AutoNotification.last).not_to eq(@last_notification)
 		end
 	end
 
 end
-
-=begin
-
-def stock_alert(product)
-		if product.stock <= product.notification_threshold
-			params = {
-				title: I18n.t('auto_notification.stock_alert.title', product: product.name),
-				text: I18n.t('auto_notification.stock_alert.text', product: product.name, stock: product.stock),
-				due_date: Datetime.now
-			}
-			self.create(params)
-		end
-	end
-
-	def balance_alert(supplier)
-		if supplier.account_balance <= supplier.notification_threshold
-			difference = supplier.notification_threshold - supplier.account_balance
-			params = {
-				due_date: Datetime.now,
-				title: I18n.t('auto_notification.balance_alert.title', supplier: supplier.name),
-				text: I18n.t('auto_notification.balance_alert.text',
-					supplier: supplier.name,
-					balance: supplier.account_balance,
-					difference: difference
-					)
-			}
-			self.create(params)
-		end
-	end
-
-=end
