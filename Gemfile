be4030f8e8c8057f4975f45f6a5897c3382192e5
Camila Pais <camila.pais97@gmail.com>
2020-06-10 16:20:04 -0300

    rebuilding views with simple forms

--- Gemfile
+++ Gemfile
@@ -54,10 +54,6 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rubocop', require: false
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,12 +73,10 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :price, :unit, :promotion_price, :stock, mass_stock: [:product_id, :stock])
     end
