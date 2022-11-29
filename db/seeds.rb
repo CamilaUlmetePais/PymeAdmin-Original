5c4845e1f25aeab4498242d69c9bc8b5ea8546a9
CamilaUlmetePais <camilaulmetepais@gmail.com>
2022-11-29 19:01:10 -0300

    added seeds to better test the new form

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,6 +39,8 @@ product6 = Product.create(
   {name: "Pechuga", price: 180, unit: "kg", stock: 100, notification_threshold: 5})
 product7 = Product.create(
   {name: "Pata y muslo", price: 150, unit: "kg", stock: 100, notification_threshold: 5})
+product8 = Product.create(
+  {name: "Miel", price: 75, unit: "u", stock: 100, notification_threshold: 5})
 SupplyProductLink.create ({product_id: 1 , supply_id: 1})
 SupplyProductLink.create ({product_id: 2, supply_id: 2})
 SupplyProductLink.create ({product_id: 3, supply_id: 3})
@@ -47,13 +49,21 @@ inflow_item2 = {quantity: 5, product_id: 4, inflow_id: 1 }    # subtotal = 5*150
 inflow_item3 = {quantity: 2.35, product_id: 5, inflow_id: 2 } # subtotal = 2.35*80=188
 inflow_item4 = {quantity: 3, product_id: 6, inflow_id: 3 }    # subtotal = 3*180=540
 inflow_item5 = {quantity: 2.5, product_id: 4, inflow_id: 3 }  # subtotal = 2.5*150=375
-inflow_item6 = {quantity: 5, product_id: 1, inflow_id: 3 }    # subtotal = 5*180=900
-inflow_item7 = {quantity: 1, product_id: 2, inflow_id: 3 }    # subtotal = 1*25 = 25
-inflow_item8 = {quantity: 2, product_id: 3, inflow_id: 3 }    # subtotal = 2*50 = 100
+inflow_item6 = {quantity: 5, product_id: 1, inflow_id: 4 }    # subtotal = 5*180=900
+inflow_item7 = {quantity: 1, product_id: 1, inflow_id: 5 }    # subtotal = 80
+inflow_item8 = {quantity: 2, product_id: 2, inflow_id: 5 }    # subtotal = 2*25=50
+inflow_item9 = {quantity: 3, product_id: 3, inflow_id: 5 }    # subtotal = 3*50=150
+inflow_item10 = {quantity: 4, product_id: 4, inflow_id: 5 }   # subtotal = 4*150=600
+inflow_item11 = {quantity: 5, product_id: 5, inflow_id: 5 }   # subtotal = 5*80=400
+inflow_item12 = {quantity: 6, product_id: 6, inflow_id: 5 }   # subtotal = 6*180=1080
+inflow_item13 = {quantity: 7, product_id: 7, inflow_id: 5 }   # subtotal = 7*150=1050
+inflow_item14 = {quantity: 8, product_id: 8, inflow_id: 5 }   # subtotal = 8*75=600
 Inflow.create({total: 2250, cash: true, inflow_items_attributes: [inflow_item1, inflow_item2] })
 Inflow.create({total: 188, cash: false, inflow_items_attributes: [inflow_item3] })
 Inflow.create({total: 915, cash: true, inflow_items_attributes: [inflow_item4, inflow_item5] })
 Inflow.create({total: 900, cash: true, inflow_items_attributes: [inflow_item6] })
+Inflow.create({total: 4010, cash: true, inflow_items_attributes: [
+  inflow_item7, inflow_item8, inflow_item9, inflow_item10, inflow_item11, inflow_item12, inflow_item13, inflow_item14]})
 outflow_item1 = {quantity: 500, supply_id: 4, outflow_id: 1 } # subtotal = 500*1=500
 outflow_item2 = {quantity: 20, supply_id: 7, outflow_id: 2 }  # subtotal = 20*100=2000
 outflow_item3 = {quantity: 2, supply_id: 8, outflow_id: 2 }   # subtotal = 2*300=600
